1bfa9b58b3496534d38cc9ffd3cd4e7fb1b40798
supriya <supriyanevewani@gmail.com>
2026-09-14 23:25:41 +0530

    Improve home page headline animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,30 @@
 
 import { Component } from '@angular/core';
+
 import { RouterLink } from '@angular/router';
 
 @Component({
+
   selector: 'app-home',
+
   standalone: true,
+
   imports: [RouterLink],
+
   templateUrl: './home.component.html',
+
   styleUrl: './home.component.css'
+
 })
+
 export class HomeComponent {
 
-  lineOne = Array.from("We don't just build software...");
-  lineTwo = Array.from("We build the people who build it!");
+  lineOne = "We don't just build software...";
+
+  lineTwo = "We build the people who build it!";
+
+  lineOneWords = this.lineOne.split(' ');
+
+  lineTwoWords = this.lineTwo.split(' ');
 
 }
